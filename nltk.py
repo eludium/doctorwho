f18800d81c5d494154af6ee4692c44cd4d3a3e6f
eludium <myraize@gmail.com>
2015-12-03 23:52:51 +0100

    Update nltk.py

--- nltk.py
+++ nltk.py
@@ -2,11 +2,13 @@
 from __future__ import division
 import nltk, re, pprint
 from nltk import word_tokenize
+import codecs
 
 # oeffne Textdatei
-f = open('test.txt', 'rU')
+f = codecs.open('12_doctor_all_words.txt', 'rU', 'UTF-8-sig')
 
-# lese Textdatei
+# die decodierung geht aber irgendwie nicht, wenn man ne .txt nimmt mit "bla bla bla" geht alles
+# aber sobald oder sonstige Sachen dabei sind, kackt et ab (auch wenn hier in den Kommentaren Umlaute sind... meh)
 raw = f.read()
 
 # txt umwandeln damit nltk damit arbeiten kann
@@ -14,4 +16,4 @@ tokens = nltk.word_tokenize(raw)
 text = nltk.Text(tokens)
 
 # nltk methode concordance aufrufen
-text.concordance('test')
+associations = text.concordance('die')
