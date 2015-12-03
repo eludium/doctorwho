a60267b4494d7e41de568014d78a424af8dd1b71
eludium <myraize@gmail.com>
2015-12-03 20:40:29 +0100

    Update nltk.py

--- nltk.py
+++ nltk.py
@@ -4,7 +4,7 @@ import nltk, re, pprint
 from nltk import word_tokenize
 
 # öffne Textdatei
-f = open('all_words_12th_doctor', 'rU')
+f = open('all_words_12th_doctor.txt', 'rU')
 
 # die decodierung geht aber irgendwie nicht, wenn man ne .txt nimmt mit "bla bla bla" geht alles
 # aber sobald Üä*+ oder sonstige Sachen dabei sind, kackt et ab
