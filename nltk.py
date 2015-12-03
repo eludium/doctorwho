129eb97c8773331fea10944ae9e7de4f26b0b5fb
eludium <myraize@gmail.com>
2015-12-03 20:34:57 +0100

    Create nltk.py

---
+++ nltk.py
@@ -0,0 +1,18 @@
+# irgend nen zeugs importieren
+from __future__ import division
+import nltk, re, pprint
+from nltk import word_tokenize
+
+# öffne Textdatei
+f = open('all_words_12th_doctor', 'rU')
+
+# die decodierung geht aber irgendwie nicht, wenn man ne .txt nimmt mit "bla bla bla" geht alles
+# aber sobald Üä*+ oder sonstige Sachen dabei sind, kackt et ab
+raw = f.read().decode('utf8')
+
+# txt umwandeln damit nltk damit arbeiten kann
+tokens = nltk.word_tokenize(raw).txt
+text = nltk.Text(tokens)
+
+# nltk methode concordance aufrufen
+text.concordance('doctor')
